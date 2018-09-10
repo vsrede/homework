3b1a7b0725e4bf2f22fe0d05361c0fbb3c6bf329
vsrede <43009317+vsrede@users.noreply.github.com>
2018-09-10 19:48:55 +0300

    Update classInArr.php

--- classInArr.php
+++ classInArr.php
@@ -1,26 +1,20 @@
 <?php
   class MyFirst
-{
-    public $arr;
-    function __construct(array $arr) {
+  {
+      public $arr;
+      public $a=5;
+      function __construct(array $arr) {
         $this->arr = $arr;
-       }
-    public function myInArr(){
-        foreach ($this->arr as $value) {
-         var_dump($this->arr);
-         $a=100;
-          if ($a===$value) {
-            $result='TRUE';
-            return $result;
-          }
-          else {
-            $result='FALSE';
-          }
-          return $result;
         }
-    }
-   }
-      $arr = [1,2,3,4,5,6,7,8];
-      $MyArray1 = new MyFirst ($arr);
-      echo $MyArray1->myInArr();
-?>
+      public function myInArr(){
+        foreach ($this->arr as $value) {
+          if ($this->a===$value) {
+            return  'TRUE';
+          }
+        }
+      }
+  }      
+  $arr = [1,2,3,4,5,6,7,8];
+  $MyArray1 = new MyFirst ($arr);
+  echo $MyArray1->myInArr();
+  ?> 
